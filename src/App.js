3289bee917da03cbe424340254ef43d40ea9f6b0
Priyammondal <priyammondal096@gmail.com>
2022-12-24 21:26:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import Home from "./components/Home.jsx";
+import "./styles/app.scss";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+};
+
+export default App;
